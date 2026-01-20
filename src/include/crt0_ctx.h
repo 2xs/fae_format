8fa9558b82d0b648f5c68ab3f9764ea56f1685d6
Gregory Guche <gregory.guche@univ-lille.fr>
2026-01-20 14:49:27 +0100

    Added a missing comment in crt0_ctx_t

--- src/include/crt0_ctx.h
+++ src/include/crt0_ctx.h
@@ -4,7 +4,7 @@
 /**
  * @brief Data structure that describes the memory layout
  * required by the CRT0 to execute the relocatable binary
- * @warning MUST REMAIN SYNCHRONIZED with xipfs's file definition.
+ * @warning MUST REMAIN SYNCHRONIZED with libpip-riot and xipfs's file definitions.
  */
 typedef struct crt0_ctx_s {
     /*
